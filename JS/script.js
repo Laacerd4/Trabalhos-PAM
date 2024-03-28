32fd37192ec93e73c9fb5c8916a2780f65abf467
Laacerd4 <152025048+Laacerd4@users.noreply.github.com>
2024-03-28 08:57:37 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,12 @@
-function mostrarAlerta() {
-    alert("Mensagem qualquer após o clique!");
+function botaoA() {
+  alert("Mensagem qualquer após o clique!");
 }
  
 
 //Trabalhando com o DOM
 //document.getElementById(): Permite selecionar um elemento pelo seu ID
 var elemento = document.getElementById('meuId');
+
 //document.getElementsByTagName(): Seleciona elementos pelo nome da tag, retornando uma coleção de elementos
 var elementos = document.getElementsByTagName('div');
 //document.getElementsByClassName(): Seleciona elementos pelo nome da classe, também retornando uma coleção de elementos.
@@ -15,18 +16,19 @@ var elemento = document.querySelector('.minhaClasse');
 //document.querySelectorAll(): Seleciona todos os elementos que correspondem a um seletor CSS, retornando uma NodeList.
 var elementos = document.querySelectorAll('div.minhaClasse');
 
+
 let contador = 0;
 document.getElementById('botao').onclick = function() {
-    contador++;
-    alert("Número de vezes clicado: " + contador);
+  contador++;
+  alert("vezes clicado: " + contador);
 };
 
-//função para converter strings para caixa alta
-function converterParaMaiuscula(str) {
-    return str.toUpperCase();
-  }
-console.log(converterParaMaiuscula("minha string"));
-  
+function maiuscula() {
+  var texto = document.getElementById("texto").value;
+  var textoConvertido = texto.toUpperCase();
+  document.getElementById("textoConvertido").innerText = textoConvertido;
+}
+
 //função para somar números
 function somar(a, b) {
     return a + b;
